91429338816f187dbb75f5bfce0e060d7b594f03
Trần Hoàng Quốc Hưng <thqhpla15@gmail.com>
2026-09-17 10:26:50 +0700

    Update Bai1.php

--- Buoi 1/Bai1.php
+++ Buoi 1/Bai1.php
@@ -1,1 +1,34 @@
+<?php
 
+$students = [
+    [
+        "name" => "Nguyen Van An",
+        "age" => 20,
+        "score" => 8.5
+    ],
+    [
+        "name" => "Tran Thi Binh",
+        "age" => 21,
+        "score" => 6.5
+    ],
+    [
+        "name" => "Le Van Cuong",
+        "age" => 19,
+        "score" => 4.5
+    ],
+    [
+        "name" => "Pham Thi Dung",
+        "age" => 20,
+        "score" => 7.5
+    ]
+];
+
+foreach ($students as $student) {
+    echo "Ho va ten: " . $student["name"] . ", tuoi: " . $student["age"] . ", diem: " . $student["score"] . PHP_EOL;
+}
+
+$totalScore = 0;
+foreach ($students as $student) {
+    $totalScore += $student["score"];
+}
+echo "Diem trung binh cua tat ca sinh vien: " . $totalScore / count($students);
